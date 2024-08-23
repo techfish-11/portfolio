a161989620db2328e394fb60fd969eb50eb54c98
iruka541 <msuke1018@icloud.com>
2024-08-23 18:05:18 +0900

    色々した

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,18 @@
-"use client";
 import Footer from "@/components/footer";
 import Future from "@/components/future";
 import Hero from "@/components/hero";
 import Header from "@/components/header";
-import MobileHome from "@/components/mobileHome";
 
 export default function Home() {
-  if (typeof window !== 'undefined' && /Mobi|Android/i.test(navigator.userAgent)) {
-    return <MobileHome />;
-  } else {
-    return (
-      <div>
-        <Header />
-        <main className="flex flex-col space-y-0" style={{ marginBottom: '-20px' }}>
-          <Hero />
-          <div className="bg-[#f3f4f6] h-16" style={{ marginBottom: '-70px', backdropFilter: 'blur(8px)' }}></div>
-          <Future />
-        </main>
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header />
+      <main className="flex flex-col space-y-0" style={{ marginBottom: '-20px' }}>
+        <Hero />
+        <div className="bg-[#f3f4f6] h-16" style={{ marginBottom: '-70px', backdropFilter: 'blur(8px)' }}></div>
+        <Future />
+      </main>
+      <Footer />
+    </div>
+  );
 }
